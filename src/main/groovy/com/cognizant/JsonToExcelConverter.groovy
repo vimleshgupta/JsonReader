2eebc9440e449e39f243123fa5e40c7bf19a678b
vishall <vishal.loke@cognizant.com>
2016-05-15 02:53:49 +0100

    Updated id and model family

--- src/main/groovy/com/cognizant/JsonToExcelConverter.groovy
+++ src/main/groovy/com/cognizant/JsonToExcelConverter.groovy
@@ -19,9 +19,9 @@ class JsonToExcelConverter {
 
     final String workbookFileName = 'Report.xls'
     final String sheetName = 'Report Worksheet'
-    final List headers = ["SKU", "Brand", "Model", "Consumer New", "Consumer Upgrade", "Voice New", "Voice Upgrade", "Stock Limited", "Life Cycle",
+    final List headers = ["ID","SKU", "Brand", "Model", "Consumer New", "Consumer Upgrade", "Voice New", "Voice Upgrade", "Stock Limited", "Life Cycle",
                           "RRP", "Replacement cost", "Is RRP & RCost equal", "Is listHalf present",
-                          "Relationship type", "Id/path", "Price one-off", "Price monthly", "Is Price & RRP equal"]
+                          "Relationship type", "Id/path", "Price one-off", "Price monthly", "Is Price & RRP equal","ModelFamily"]
 
     JsonToExcelConverter(String path) {
         this.path = path
@@ -50,30 +50,32 @@ class JsonToExcelConverter {
 
             def excelRows = [
                     mainRow : [
-                            newCell(0, data.sku.code, verticalCenterFormat),
-                            newCell(1, data.brand, verticalCenterFormat),
-                            newCell(2, data.model, verticalCenterFormat),
-                            newCell(3, data.channelPermissions.ConsumerNew, verticalCenterFormat),
-                            newCell(4, data.channelPermissions.ConsumerUpgrade, verticalCenterFormat),
-                            newCell(5, data.channelPermissions.VoiceNew, verticalCenterFormat),
-                            newCell(6, data.channelPermissions.VoiceUpgrade, verticalCenterFormat),
-                            newCell(7, getResult(data.stockLimited), verticalCenterFormat),
-                            newCell(8, data.lifecycle.status, verticalCenterFormat),
-                            newCell(9, data.rrp, verticalCenterFormat),
-                            newCell(10, data.replacementCost, verticalCenterFormat),
-                            newCell(11, getResult(data.replacementCost == data.rrp), verticalCenterFormat),
-                            newCell(12, data.id == data.leadModelInFamily ? getResult(data.images.standard.listHalf) : "Not a lead model in family", verticalCenterFormat),
+                            newCell(0, data.id, verticalCenterFormat),
+                            newCell(1, data.sku.code, verticalCenterFormat),
+                            newCell(2, data.brand, verticalCenterFormat),
+                            newCell(3, data.model, verticalCenterFormat),
+                            newCell(4, data.channelPermissions.ConsumerNew, verticalCenterFormat),
+                            newCell(5, data.channelPermissions.ConsumerUpgrade, verticalCenterFormat),
+                            newCell(6, data.channelPermissions.VoiceNew, verticalCenterFormat),
+                            newCell(7, data.channelPermissions.VoiceUpgrade, verticalCenterFormat),
+                            newCell(8, getResult(data.stockLimited), verticalCenterFormat),
+                            newCell(9, data.lifecycle.status, verticalCenterFormat),
+                            newCell(10, data.rrp, verticalCenterFormat),
+                            newCell(11, data.replacementCost, verticalCenterFormat),
+                            newCell(12, getResult(data.replacementCost == data.rrp), verticalCenterFormat),
+                            newCell(13, data.id == data.leadModelInFamily ? getResult(data.images.standard.listHalf) : "Not a lead model in family", verticalCenterFormat),
+                            newCell(19, data.modelFamily, verticalCenterFormat),
                     ],
                     subRows : []
             ]
 
             relationships.each {
                 def subRow = [
-                        newCell(13, it.type),
-                        newCell(14, it.id),
-                        newCell(15, it.prices.oneOff.toString()),
-                        newCell(16, it.prices[0].monthly ? it.prices.monthly.toString() : "NA"),
-                        newCell(17, it.id.contains("prepaySims") ? getResult(it.prices[0].oneOff == data.rrp) : "Not a prepay sims")
+                        newCell(14, it.type),
+                        newCell(15, it.id),
+                        newCell(16, it.prices.oneOff.toString()),
+                        newCell(17, it.prices[0].monthly ? it.prices.monthly.toString() : "NA"),
+                        newCell(18, it.id.contains("prepaySims") ? getResult(it.prices[0].oneOff == data.rrp) : "Not a prepay sims")
                 ]
                 excelRows.subRows << subRow
             }
